a2fee1444ce119bef18642a613d7e44c7308e9c9
rulyadhika <rulihanif@gmail.com>
2020-08-03 08:29:53 +0700

    adding event to window, and adjusting card image

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function reqData() {
-  fetch("http://hp-api.herokuapp.com/api/characters")
+  return fetch("http://hp-api.herokuapp.com/api/characters")
     .then((result) => result.json())
     .then((result) => result);
 }
@@ -7,18 +7,23 @@ function reqData() {
 function displayData(data) {
   let getAllData = data
     .map(
-      (d) => `<div class="col-md-4">
-                <div class="card" style="width: 18rem;">
-                <img src="${d.image}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${d.name}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+      (d) => `<div class="col-md-3 my-4">
+                <div class="card">
+                    <img src="${d.image}" class="card-img-top card-image" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${d.name}</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
                 </div>
-            </div>
-            </div>`
+               </div>`
     )
     .join("");
   const cardContainer = document.querySelector(".card-container");
   cardContainer.innerHTML = getAllData;
 }
+
+window.addEventListener("load", async function () {
+  const data = await reqData();
+  displayData(data);
+});
