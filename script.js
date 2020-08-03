1d4a688c9b10900e2d70e1112f1094906c180636
rulyadhika <rulihanif@gmail.com>
2020-08-03 09:53:50 +0700

    adding slight touch to character card

--- script.js
+++ script.js
@@ -8,12 +8,15 @@ function displayData(data) {
   let getAllData = data
     .map(
       (d) => `<div class="col-md-3 my-4">
-                <div class="card">
+                <div class="card characterCard">
                     <img src="${d.image}" class="card-img-top card-image" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${d.name}</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <ul>
+                            <li>Species : ${d.species}</li>
+                            <li>House : ${d.house}</li>
+                        </ul>
+                        <a href="#" class="btn btn-info">More Detail</a>
                     </div>
                 </div>
                </div>`
